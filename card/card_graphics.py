8e2e61d42171ce1c9b71e618bb09d1f4800bb734
TheRockerfly <jamesrocker@hotmail.co.uk>
2020-12-20 19:43:53 +0000

    fix snow, background and font

--- card/card_graphics.py
+++ card/card_graphics.py
@@ -1,5 +1,4 @@
 import turtle
-import time
 import random
 
 
@@ -9,44 +8,47 @@ def make_ball(x, y, size, colour, ball):
     ball.setposition(x, y)
     ball.color(colour)
     ball.dot(size)
+    print(ball.position())
 
 
 class Card:
     def __init__(self):
         #  setup the screen to 900 by 900 and two turtles for the circle baubles and squares for tree
         self.screen = turtle.Screen()
-        self.screen.bgcolor("black")
+        self.screen.bgcolor("#02053b")
         self.screen.setup(900, 900)
         self.star = turtle.Turtle()
         self.turtle_text = turtle.Turtle()
         self.turtle_text.hideturtle()  # we want to initialise the turtle but hide it for later
         self.snow_size = 7
+        self.list_of_snow = []
 
     def move_snow(self, snow):
         snow_speed = 2
         position = snow.position()
         snow.clear()
-        make_ball(position[0], position[1] - snow_speed, self.snow_size, "white", snow)
+        x_axis = position[0]
+        y_axis = position[1] - snow_speed
+        make_ball(x_axis, y_axis, self.snow_size, "white", snow)
 
     def let_it_snow(self):
-        rate_of_snow_balls = 1
+        rate_of_snow_balls = 2
         rand_make_snow = random.randint(0, rate_of_snow_balls)
         width = 900
-        list_of_snow = []
 
         if rand_make_snow == 0:
             snow = turtle.Turtle()  # initialise a snowball
             snow.hideturtle()
             snow.penup()
-            list_of_snow.append(snow)  # make a list of snowballs
+            self.list_of_snow.append(snow)  # make a list of snowballs
             make_ball(random.randint(int(-width / 2), int(width / 2)), width / 2, self.snow_size, "white", snow)
 
-            for snow in list_of_snow:
-                self.move_snow(snow)
-                if snow.position()[1] <= -width / 2:
-                    snow.clear()
-                    list_of_snow.remove(snow)
-                    del snow
+        for snow in self.list_of_snow:
+            self.move_snow(snow)
+            if snow.position()[1] <= -width / 2:  # This is so when the snow meets the half way it disappears
+                snow.clear()
+                self.list_of_snow.remove(snow)
+                del snow
 
         self.screen.update()
 
@@ -68,24 +70,25 @@ class Card:
         self.star.end_fill()
 
     def build_cover(self):
+        self.build_star()
+        font_style = ("Elephant", 30, "bold")
+        self.turtle_text.penup()  # want to pen up so we don't draw a line moving the turtle to text block
 
+        # configs
+        self.turtle_text.speed('fastest')
+        self.turtle_text.setpos(0, 350)
+        self.turtle_text.color('red')
+        self.turtle_text.pendown()
+        self.turtle_text.write('Holiday Wishes', font=font_style, align='center')
+        self.turtle_text.hideturtle()
 
-        # self.build_star()
-        # font_style = ('Apple Chancery', 30, "normal")
-        # self.turtle_text.penup()  # want to pen up so we don't draw a line moving the turtle to text block
-        #
-        # # configs
-        # self.turtle_text.speed('fastest')
-        # self.turtle_text.setpos(0, 350)
-        # self.turtle_text.color('red')
-        # self.turtle_text.pendown()
-        # self.turtle_text.write('Holiday Wishes', font=font_style, align='center')
-        # self.turtle_text.hideturtle()
-        #
-        # # turtle.Screen().exitonclick()
         self.screen.tracer(0)
-        for _ in range(50):
+        for _ in range(25):
             self.let_it_snow()
-        time.sleep(5)
+        for _ in range(25):
+            self.let_it_snow()
+        for _ in range(500):
+            self.let_it_snow()
+
         turtle.clear()
         turtle.clearscreen()
