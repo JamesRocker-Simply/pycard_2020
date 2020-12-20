5086675fbb764841d2ecb06bad1dfaf49cc9fb49
James Rocker <james.rocker@simplybusiness.co.uk>
2020-12-20 16:44:17 +0000

    remove args that are no longer required

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ if __name__ == '__main__':
     cli_arguments = docopt(__doc__)
     user = str(os.getlogin()).replace('.', ' ').capitalize()
     if cli_arguments.get('--cover', True):
-        cg.Card().build_cover(user)
+        cg.Card().build_cover()
 
     if cli_arguments.get('--inside', True):
         ci.InternalCard().build_internal(user)
 
     if cli_arguments.get('--card', True):
-        cg.Card().build_cover(user)
+        cg.Card().build_cover()
         ci.InternalCard().build_internal(user)
